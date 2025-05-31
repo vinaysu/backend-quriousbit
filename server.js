a4ed380a3828c87d9f668ef70a53b9077f363364
vinaysu <suddakuntivinay7877@gmail.com>
2025-05-31 18:11:19 +0530

    images added

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const servers = [
     tickrate: "60 Hz",
     players: "60/64",
     ping: 104,
-    logo: "https://example.com/logo1.jpg",
+    logo: "https://i.pinimg.com/736x/98/4a/10/984a10ae157f96bbe6c2afcde96bf3d7.jpg",
     country: "US"
   },
   {
@@ -22,7 +22,7 @@ const servers = [
     tickrate: "40 Hz",
     players: "60/64",
     ping: 107,
-    logo: "https://example.com/logo2.jpg",
+    logo: "https://t4.ftcdn.net/jpg/08/19/40/49/240_F_819404947_wdWfh5v7qA2Sbz0KgI2coUyt4KaV6OWO.jpg",
     country: "US"
   },
   {
@@ -32,7 +32,7 @@ const servers = [
     tickrate: "60 Hz",
     players: "60/64",
     ping: 107,
-    logo: "https://example.com/logo3.jpg",
+    logo: "https://www.shutterstock.com/image-photo/indian-national-flag-hd-wallpaper-260nw-2197211151.jpg",
     country: "DE"
   },
   {
@@ -42,7 +42,7 @@ const servers = [
     tickrate: "30 Hz",
     players: "60/64",
     ping: 47,
-    logo: "https://example.com/logo4.jpg",
+    logo: "https://t4.ftcdn.net/jpg/08/28/97/97/240_F_828979767_f8OMlrw28fxkOBaTdFDmALfsUaU68W1B.jpg",
     country: "FR"
   },
   {
@@ -52,7 +52,7 @@ const servers = [
     tickrate: "30 Hz",
     players: "61/64",
     ping: 47,
-    logo: "https://example.com/logo5.jpg",
+    logo: "https://t3.ftcdn.net/jpg/06/34/72/08/240_F_634720826_Gqc0dvkaaAFLdxZzUtgFc9U9SPcThaz4.jpg",
     country: "DE"
   },
   {
@@ -62,7 +62,7 @@ const servers = [
     tickrate: "30 Hz",
     players: "61/64",
     ping: 47,
-    logo: "https://example.com/logo6.jpg",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH-1q5Fw11E9Lkg6fDp6Uf6jmbT4DhlZbhxg&s",
     country: "DE"
   },
   {
@@ -72,7 +72,7 @@ const servers = [
     tickrate: "60 Hz",
     players: "61/64",
     ping: 47,
-    logo: "https://example.com/logo7.jpg",
+    logo: "https://t4.ftcdn.net/jpg/09/06/45/23/240_F_906452312_VgTwqGASxyY30tcvNI2YHczUzV8TU3OY.jpg",
     country: "US"
   },
   {
@@ -82,7 +82,7 @@ const servers = [
     tickrate: "30 Hz",
     players: "61/64",
     ping: 47,
-    logo: "https://example.com/logo8.jpg",
+    logo: "https://t4.ftcdn.net/jpg/10/14/16/61/240_F_1014166150_6YC2RoKjI4FBP9csY3Sok1EiFTDimdnW.jpg",
     country: "GB"
   },
   {
@@ -92,7 +92,7 @@ const servers = [
     tickrate: "60 Hz",
     players: "61/64",
     ping: 186,
-    logo: "https://example.com/logo9.jpg",
+    logo: "https://t3.ftcdn.net/jpg/06/44/97/44/240_F_644974401_VxF5h0SiSL06oyyQsp9LOFEhtCuWd1h9.jpg",
     country: "FR"
   },
   {
@@ -102,7 +102,7 @@ const servers = [
     tickrate: "60 Hz",
     players: "61/64",
     ping: 47,
-    logo: "https://example.com/logo10.jpg",
+    logo: "https://t3.ftcdn.net/jpg/08/28/97/98/240_F_828979816_DMHNStqEi7jQasWtKx7PAMSyHxsoClK6.jpg",
     country: "DE"
   },
   {
@@ -112,7 +112,7 @@ const servers = [
     tickrate: "30 Hz",
     players: "62/64",
     ping: 47,
-    logo: "https://example.com/logo11.jpg",
+    logo: "https://t4.ftcdn.net/jpg/08/42/87/33/240_F_842873394_nhuzyFZTXohWQbXnA55fXnRcKBsuWoJG.jpg",
     country: "DE"
   },
   {
@@ -122,7 +122,7 @@ const servers = [
     tickrate: "60 Hz",
     players: "63/64",
     ping: 47,
-    logo: "https://example.com/logo12.jpg",
+    logo: "https://t4.ftcdn.net/jpg/06/66/23/61/240_F_666236196_Mm511rmpUDjqQ0YpZItXrYB47wp19JdS.jpg",
     country: "DE"
   },
   {
@@ -132,7 +132,7 @@ const servers = [
     tickrate: "60 Hz",
     players: "63/64",
     ping: 47,
-    logo: "https://example.com/logo13.jpg",
+    logo: "https://t4.ftcdn.net/jpg/08/19/40/49/240_F_819404947_wdWfh5v7qA2Sbz0KgI2coUyt4KaV6OWO.jpg",
     country: "DE"
   },
   {
@@ -142,7 +142,7 @@ const servers = [
     tickrate: "40 Hz",
     players: "63/64",
     ping: 47,
-    logo: "https://example.com/logo14.jpg",
+    logo: "https://t4.ftcdn.net/jpg/06/66/23/61/240_F_666236196_Mm511rmpUDjqQ0YpZItXrYB47wp19JdS.jpg",
     country: "DE"
   }
 ];
